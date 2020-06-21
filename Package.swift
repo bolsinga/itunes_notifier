4d9751ff10d7560a22884bde80dfae0f9bc72686
Greg Bolsinga <bolsinga@mac.com>
2020-06-20 21:07:02 -0700

    Add Swift Package for itunes notifcation to json tool
    - This tool is passed a directory, where it will create the file "lastSong.json".
    - This json represents the last song played by iTunes on the mac the tool is running on.
    - It listens for the `DistributedNotificationCenter` to post to the `com.apple.iTunes.playerInfo` notification.
    - The code will remove "transient" keys about UI state. This way the json is consistent when pausing songs while they are playing.

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "itunes_notifier",
+    platforms: [
+        .macOS(.v10_14),
+    ],
+    dependencies: [
+//        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.1.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "itunes_notifier",
+            dependencies: []),
+    ]
+)
---
+++ Sources/itunes_notifier/main.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+if CommandLine.arguments.count < 2 {
+    print("Must provide a destination directory")
+    exit(1)
+}
+
+let destinationDirectoryPath = CommandLine.arguments[1]
+let destinationDirectoryURL = URL(fileURLWithPath: destinationDirectoryPath, isDirectory: true)
+
+func save(jsonData : Data) throws {
+    let fm = FileManager.default;
+
+    let filename = "lastSong.json"
+    let temporaryURL = URL(fileURLWithPath: filename, relativeTo: fm.temporaryDirectory)
+    let destinationURL = URL(fileURLWithPath: filename, relativeTo: destinationDirectoryURL)
+
+    try jsonData.write(to: temporaryURL, options: .atomic)
+    if (!fm.contentsEqual(atPath: destinationURL.path, andPath: temporaryURL.path)) {
+        do {
+            try fm.removeItem(at: destinationURL)
+        } catch {}
+        try fm.moveItem(at: temporaryURL, to: destinationURL)
+    } else {
+        try fm.removeItem(at: temporaryURL)
+    }
+}
+
+DistributedNotificationCenter.default().addObserver(forName: NSNotification.Name("com.apple.iTunes.playerInfo"), object: nil, queue: nil) { (notification) in
+    guard notification.object as? String == "com.apple.iTunes.player" else {
+        print("Unknown Notification.object : \(notification)")
+        return
+    }
+
+    guard let t = notification.userInfo else {
+        print("Empty Notification.userInfo : \(notification)")
+        return
+    }
+
+    // Make the dictionary a real [String : String] so the following filter is simpler.
+    // This has the some UI state that is not desirable, so disallow those keys.
+    // This UI state is toggling, and it is not of interest to code that is just interesting in what is playing.
+    let info = Dictionary(uniqueKeysWithValues: t.map {
+        key, value in (String(describing: key), String(describing: value))
+    }).filter { (key, _) in key != "Player State" && key != "Back Button State" }
+
+    guard JSONSerialization.isValidJSONObject(info) else {
+        print("Unabled to create JSON for \(notification)")
+        return
+    }
+
+    do {
+        try save(jsonData: JSONSerialization.data(withJSONObject: info, options: [.prettyPrinted, .sortedKeys]))
+    } catch {
+        print("Error Creating JSON: \(error) for: \(notification)")
+    }
+}
+
+RunLoop.main.run()
