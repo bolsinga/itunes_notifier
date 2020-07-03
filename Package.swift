50aa444d2d3dc985473809e0254c2a4bef72fb26
Greg Bolsinga <bolsinga@mac.com>
2020-07-02 20:15:47 -0700

    Use swift-log to loger errors while running (#4)
    Fixes #3

--- Package.swift
+++ Package.swift
@@ -10,12 +10,13 @@ let package = Package(
     ],
     dependencies: [
 //        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.1.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "itunes_notifier",
-            dependencies: []),
+            dependencies: ["Logging"]),
     ]
 )
