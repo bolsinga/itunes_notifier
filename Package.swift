58e17e68f0cf62a5e5dbd15020ba6c0e8a789cc3
Greg Bolsinga <bolsinga@mac.com>
2021-01-09 11:04:19 -0800

    Fix dependency after package manager update

--- Package.swift
+++ Package.swift
@@ -17,6 +17,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "itunes_notifier",
-            dependencies: ["Logging"]),
+            dependencies: [.product(name: "Logging", package: "swift-log")]),
     ]
 )
