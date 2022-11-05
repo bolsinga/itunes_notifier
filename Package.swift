b08c96cd52697ff5ade3c55fe544e83e0e2e166b
Greg Bolsinga <bolsinga@mac.com>
2022-11-05 11:16:08 -0700

    Update swift and macos versions (#10)
    - Swift 5.7
    - Target macOS 12

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "itunes_notifier",
     platforms: [
-        .macOS(.v11),
+        .macOS(.v12),
     ],
     dependencies: [
 //        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.1.0"),
